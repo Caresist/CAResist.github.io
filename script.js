bb68d4cb7f559c429c72c1c603d5a381cef2c926
Anonymus1 <AnonymusGithub@gmail.com>
2015-08-12 09:04:37 -0400

    stuff and things

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-    $('#button').mouseenter(function(){$('#button').fadeTo("fast",1.0);}, 
-        function(){$('#button').fadeTo("fast",0.5);});
+    $('#button').mouseenter(function(){$('#button').fadeTo("fast",1.0);});
+    $("#button").mouseleave(function(){$('#button').fadeTo('fast',0.5);});
 });
